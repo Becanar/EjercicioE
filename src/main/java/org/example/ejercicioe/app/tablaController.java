7bd22c1d5f9f24d7a3f0cc67dc73c41c675b90ab
Becanar <benatortubitarte@gmail.com>
2024-10-24 13:27:41 +0200

    Javadoc escrito

--- src/main/java/org/example/ejercicioe/app/tablaController.java
+++ src/main/java/org/example/ejercicioe/app/tablaController.java
@@ -57,6 +57,7 @@ public class tablaController {
     private Button btnCancelar;
 
     private Stage modal;
+
     /**
      * Inicializa las columnas de la tabla con los valores de los atributos de la clase Persona.
      * Este método se ejecuta automáticamente al cargar la vista FXML.
@@ -82,7 +83,6 @@ public class tablaController {
         btnCancelar.setOnAction(actionEvent -> cancelar());
     }
 
-
     /**
      * Muestra una alerta de error con una lista de mensajes de error.
      * @param lst Lista de errores a mostrar en la alerta.
@@ -92,13 +92,14 @@ public class tablaController {
         alert.initOwner(btAgregar.getScene().getWindow());
         alert.setHeaderText(null);
         alert.setTitle("Error");
-        String error="";
-        for(String str:lst) {
-            error+=str+"\n";
+        String error = "";
+        for (String str : lst) {
+            error += str + "\n";
         }
         alert.setContentText(error);
         alert.showAndWait();
     }
+
     /**
      * Muestra una ventana de confirmación indicando que la persona fue agregada correctamente.
      */
@@ -110,12 +111,14 @@ public class tablaController {
         alerta.setContentText("Persona agregada correctamente.");
         alerta.showAndWait();
     }
+
     /**
      * Muestra una ventana modal para introducir los datos de una nueva persona.
      *
      * @param ventanaPrincipal Stage principal que servirá de propietario del modal.
+     * @param esModif Indica si la operación es para modificar una persona existente o agregar una nueva.
      */
-    public void mostrarVentanaDatos( Stage ventanaPrincipal,boolean esModif) {
+    public void mostrarVentanaDatos(Stage ventanaPrincipal, boolean esModif) {
         modal = new Stage();
         modal.setResizable(false);
         try {
@@ -132,24 +135,27 @@ public class tablaController {
         gridPane.setVgap(10);
         gridPane.getColumnConstraints().addAll(new ColumnConstraints(75), new ColumnConstraints(200));
         Label lblNombre = new Label("Nombre");
-        if(esModif){
-            txtNombre=new TextField(tablaVista.getSelectionModel().getSelectedItem().getNombre());
-        }else{
-        txtNombre = new TextField();}
+        if (esModif) {
+            txtNombre = new TextField(tablaVista.getSelectionModel().getSelectedItem().getNombre());
+        } else {
+            txtNombre = new TextField();
+        }
         gridPane.add(lblNombre, 0, 0, 1, 1);
         gridPane.add(txtNombre, 1, 0, 1, 1);
         Label lblApellidos = new Label("Apellidos");
-        if(esModif){
-            txtApellidos=new TextField(tablaVista.getSelectionModel().getSelectedItem().getApellidos());
-        }else{
-            txtApellidos = new TextField();}
+        if (esModif) {
+            txtApellidos = new TextField(tablaVista.getSelectionModel().getSelectedItem().getApellidos());
+        } else {
+            txtApellidos = new TextField();
+        }
         gridPane.add(lblApellidos, 0, 1, 1, 1);
         gridPane.add(txtApellidos, 1, 1, 1, 1);
         Label lblEdad = new Label("Edad");
-        if(esModif){
-            txtEdad=new TextField(""+tablaVista.getSelectionModel().getSelectedItem().getEdad());
-        }else{
-            txtEdad = new TextField();}
+        if (esModif) {
+            txtEdad = new TextField("" + tablaVista.getSelectionModel().getSelectedItem().getEdad());
+        } else {
+            txtEdad = new TextField();
+        }
         txtEdad.setMaxWidth(75);
         gridPane.add(lblEdad, 0, 2, 1, 1);
         gridPane.add(txtEdad, 1, 2, 1, 1);
@@ -162,15 +168,14 @@ public class tablaController {
         Scene scene = new Scene(gridPane, 300, 150);
         modal.setScene(scene);
         modal.setResizable(false);
-        if(esModif){
-            modal.setTitle("Editar Persona");
-        }else{
-        modal.setTitle("Nueva Persona");}
+        modal.setTitle(esModif ? "Editar Persona" : "Nueva Persona");
         modal.show();
     }
+
     /**
      * Guarda una nueva persona a la tabla si los datos son válidos.
      * Si la persona ya existe, muestra un error.
+     * @param esModificar Indica si la acción es para modificar una persona existente.
      */
     public void guardar(boolean esModificar) {
         boolean resultado = valido();
@@ -178,14 +183,12 @@ public class tablaController {
             Persona p = new Persona(txtNombre.getText(), txtApellidos.getText(), Integer.parseInt(txtEdad.getText()));
 
             if (esModificar) {
-
                 int selectedIndex = tablaVista.getSelectionModel().getSelectedIndex();
                 if (selectedIndex >= 0) {
                     tablaVista.getItems().set(selectedIndex, p);
                     mostrarVentanaModificado();
                 }
             } else {
-
                 if (personas.contains(p)) {
                     ArrayList<String> lst = new ArrayList<>();
                     lst.add("La persona ya existe.");
@@ -201,7 +204,6 @@ public class tablaController {
         }
     }
 
-
     /**
      * Muestra una ventana de confirmación indicando que la persona fue modificada correctamente.
      */
@@ -215,42 +217,45 @@ public class tablaController {
     }
 
     /**
-     * Cierra la ventana modal sin añadir ninguna persona.
+     * Cierra la ventana modal sin añadir ni modificar ninguna persona.
      */
     public void cancelar() {
         modal.close();
     }
+
     /**
      * Verifica si los datos introducidos son válidos.
      * Muestra errores si los campos están vacíos o si el valor de edad no es un número.
      *
      * @return true si los datos son válidos, false en caso contrario.
      */
-    private boolean valido(){
-        boolean error=false;
-        ArrayList<String> errores=new ArrayList<>();
-        if(txtNombre.getText().equals("")){
+    private boolean valido() {
+        boolean error = false;
+        ArrayList<String> errores = new ArrayList<>();
+        if (txtNombre.getText().equals("")) {
             errores.add("El campo Nombre es obligatorio.");
-            error=true;
+            error = true;
         }
-        if(txtApellidos.getText().equals("")){
+        if (txtApellidos.getText().equals("")) {
             errores.add("El campo Apellidos es obligatorio.");
-            error=true;
+            error = true;
         }
-        try{Integer.parseInt(txtEdad.getText());} catch (NumberFormatException e) {
+        try {
+            Integer.parseInt(txtEdad.getText());
+        } catch (NumberFormatException e) {
             errores.add("El campo Edad debe ser numérico.");
-            error=true;
+            error = true;
         }
-        if(error){
+        if (error) {
             mostrarAlertError(errores);
             return false;
-
         }
         return true;
     }
+
     /**
      * Método que se ejecuta al hacer clic en el botón para eliminar una persona.
-     * Verifica si hay una persona seleccionada y, si lo está elimina la persona de la tabla.
+     * Verifica si hay una persona seleccionada y, si lo está, elimina la persona de la tabla.
      * @param event Evento de acción asociado al clic del botón.
      */
     @FXML
@@ -270,7 +275,6 @@ public class tablaController {
         }
     }
 
-
     /**
      * Muestra una ventana de confirmación indicando que la persona fue eliminada correctamente.
      */
@@ -283,6 +287,11 @@ public class tablaController {
         alerta.showAndWait();
     }
 
+    /**
+     * Método que se ejecuta al hacer clic en el botón para modificar una persona.
+     * Muestra una ventana modal con los datos de la persona seleccionada para modificar.
+     * @param event Evento de acción asociado al clic del botón.
+     */
     @FXML
     void modificar(ActionEvent event) {
         Persona p = tablaVista.getSelectionModel().getSelectedItem();
